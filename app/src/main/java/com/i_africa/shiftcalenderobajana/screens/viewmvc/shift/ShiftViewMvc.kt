9331a43999accf0fa01ea828267e04d952afdf4b
iykeafrica <ikechiucheagwu@gmail.com>
2021-08-13 14:03:36 +0100

    Refactored ShiftViewMvc - ShiftMonthly Work days computation taken to another class

--- app/src/main/java/com/i_africa/shiftcalenderobajana/screens/viewmvc/shift/ShiftViewMvc.kt
+++ app/src/main/java/com/i_africa/shiftcalenderobajana/screens/viewmvc/shift/ShiftViewMvc.kt
@@ -7,15 +7,10 @@ import android.view.ViewGroup
 import com.i_africa.shiftcalenderobajana.databinding.ActivityShiftBinding
 import com.i_africa.shiftcalenderobajana.screens.viewmvc.BaseViewMvc
 import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.DateFormatter
-import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftCollection
 import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftCollection.setCollection
-import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftDuty
 import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftDuty.computeShiftDuty
-import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftUtil
+import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftMonthlyWorkDays.computeShiftDutyDays
 import com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils.ShiftUtil.setFormula
-import com.i_africa.shiftcalenderobajana.utils.Constant.FIRST_OFF
-import com.i_africa.shiftcalenderobajana.utils.Constant.SECOND_OFF
-import com.i_africa.shiftcalenderobajana.utils.Constant.THIRD_OFF
 import java.util.*
 
 private const val TAG = "ShiftViewMvc"
@@ -80,84 +75,27 @@ class ShiftViewMvc(
         day = cal.get(Calendar.DAY_OF_MONTH).toString()
         month = (cal.get(Calendar.MONTH) + 1).toString()
         year = cal.get(Calendar.YEAR).toString()
+        daysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH)
     }
 
     fun getShiftDutyInfoOnResume(shift: String) {
         setCalendar(calOne)
-        daysInMonth = calOne.getActualMaximum(Calendar.DAY_OF_MONTH)
         Log.d(TAG, "getShiftDutyInfoOnResume: $day/$month/$year")
 
-        setShiftDuty(shift)
-
-        computeShiftDutyDays(setCollection(shift))
-
-        binding.workingDaysView.text = "${DateFormatter.month(dateTwo)} has $count working days."
-        count = 0
+        setShiftDutyInfo(shift)
     }
 
     fun getShiftDutyInfoOnCalenderClick(shift: String) {
         setCalendar(calTwo)
-        daysInMonth = calTwo.getActualMaximum(Calendar.DAY_OF_MONTH)
         Log.d(TAG, "getOnCalendarClickShiftDuty: $day/$month/$year")
 
-        setShiftDuty(shift)
-
-        if (count == 0) {
-            computeShiftDutyDays(setCollection(shift))
-            binding.workingDaysView.text =
-                "${DateFormatter.month(dateTwo)} has $count working days."
-            count = 0
-        }
+        setShiftDutyInfo(shift)
     }
 
-    private fun setShiftDuty(shift: String) {
+    private fun setShiftDutyInfo(shift: String) {
         binding.shiftView.text = computeShiftDuty(setFormula(day, month, year), setCollection(shift))
+        count = computeShiftDutyDays(daysInMonth, month, year, setCollection(shift))
+        binding.workingDaysView.text = "${DateFormatter.month(dateTwo)} has $count working days."
     }
-
-    private fun computeShiftDutyDays(collection: List<String>) {
-
-        for (i in 1..daysInMonth) {
-            val dateDifference = ShiftUtil.setFormula(i.toString(), month, year)
-
-            if (dateDifference == 0 && collection[0] != FIRST_OFF && collection[0] != SECOND_OFF && collection[0] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[0]}")
-                count++
-            }
-            if (dateDifference == 1 && collection[1] != FIRST_OFF && collection[1] != SECOND_OFF && collection[1] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[1]}")
-                count++
-            }
-            if (dateDifference == 2 && collection[2] != FIRST_OFF && collection[2] != SECOND_OFF && collection[2] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[2]}")
-                count++
-            }
-            if (dateDifference == 3 && collection[3] != FIRST_OFF && collection[3] != SECOND_OFF && collection[3] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[3]}")
-                count++
-            }
-            if (dateDifference == 4 && collection[4] != FIRST_OFF && collection[4] != SECOND_OFF && collection[4] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[4]}")
-                count++
-            }
-            if (dateDifference == 5 && collection[5] != FIRST_OFF && collection[5] != SECOND_OFF && collection[5] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[5]}")
-                count++
-            }
-            if (dateDifference == 6 && collection[6] != FIRST_OFF && collection[6] != SECOND_OFF && collection[6] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[6]}")
-                count++
-            }
-            if (dateDifference == 7 && collection[7] != FIRST_OFF && collection[7] != SECOND_OFF && collection[7] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[7]}")
-                count++
-            }
-            if (dateDifference == 8 && collection[8] != FIRST_OFF && collection[8] != SECOND_OFF && collection[8] != THIRD_OFF) {
-                Log.d(TAG, "computeShiftDutyDays: ${collection[8]}")
-                count++
-            }
-        }
-        Log.d(TAG, "computeShiftDutyDays: $count")
-    }
-
 
 }
--- app/src/main/java/com/i_africa/shiftcalenderobajana/screens/viewmvc/shift/utils/ShiftMonthlyWorkDays.kt
+++ app/src/main/java/com/i_africa/shiftcalenderobajana/screens/viewmvc/shift/utils/ShiftMonthlyWorkDays.kt
@@ -1,4 +1,64 @@
 package com.i_africa.shiftcalenderobajana.screens.viewmvc.shift.utils
 
+import android.util.Log
+import com.i_africa.shiftcalenderobajana.utils.Constant
+
+private val TAG = ShiftMonthlyWorkDays::class.simpleName
+
 object ShiftMonthlyWorkDays {
+
+    private var count: Int = 0
+
+    fun computeShiftDutyDays(
+        daysInMonth: Int,
+        month: String,
+        year: String,
+        collection: List<String>
+    ): Int {
+        count = 0
+        for (i in 1..daysInMonth) {
+
+            val dateDifference = ShiftUtil.setFormula(i.toString(), month, year)
+
+            if (dateDifference == 0 && collection[0] != Constant.FIRST_OFF && collection[0] != Constant.SECOND_OFF && collection[0] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[0]}")
+                count++
+            }
+            if (dateDifference == 1 && collection[1] != Constant.FIRST_OFF && collection[1] != Constant.SECOND_OFF && collection[1] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[1]}")
+                count++
+            }
+            if (dateDifference == 2 && collection[2] != Constant.FIRST_OFF && collection[2] != Constant.SECOND_OFF && collection[2] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[2]}")
+                count++
+            }
+            if (dateDifference == 3 && collection[3] != Constant.FIRST_OFF && collection[3] != Constant.SECOND_OFF && collection[3] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[3]}")
+                count++
+            }
+            if (dateDifference == 4 && collection[4] != Constant.FIRST_OFF && collection[4] != Constant.SECOND_OFF && collection[4] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[4]}")
+                count++
+            }
+            if (dateDifference == 5 && collection[5] != Constant.FIRST_OFF && collection[5] != Constant.SECOND_OFF && collection[5] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[5]}")
+                count++
+            }
+            if (dateDifference == 6 && collection[6] != Constant.FIRST_OFF && collection[6] != Constant.SECOND_OFF && collection[6] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[6]}")
+                count++
+            }
+            if (dateDifference == 7 && collection[7] != Constant.FIRST_OFF && collection[7] != Constant.SECOND_OFF && collection[7] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[7]}")
+                count++
+            }
+            if (dateDifference == 8 && collection[8] != Constant.FIRST_OFF && collection[8] != Constant.SECOND_OFF && collection[8] != Constant.THIRD_OFF) {
+                Log.d(TAG, "computeShiftDutyDays: ${collection[8]}")
+                count++
+            }
+        }
+        Log.d(TAG, "computeShiftDutyDays: $count")
+        return count
+    }
+
 }
