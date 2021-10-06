49b0c62d3ade5d0f2c76595285a7e359911fffbc
iykeafrica <ikechiucheagwu@gmail.com>
2021-10-06 21:58:37 +0100

    SplashScreen modified to simple theme

--- app/src/main/java/com/i_africa/shiftcalenderobajana/screens/SplashScreenActivity.kt
+++ app/src/main/java/com/i_africa/shiftcalenderobajana/screens/SplashScreenActivity.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.i_africa.shiftcalenderobajana.R
 import com.i_africa.shiftcalenderobajana.databinding.ActivitySplashScreenBinding
+import com.i_africa.shiftcalenderobajana.screens.selectshiftall.SelectShiftAllActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -19,42 +21,11 @@ class SplashScreenActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@SplashScreenActivity, SecondActivity::class.java))
-            }
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
-
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) { }
-
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) { }
-        })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.motionLayout.startLayoutAnimation()
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, SelectShiftAllActivity::class.java))
+            finish()
+        }, 300)
 
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUIAndNavigation(this)
-        }
-    }
-
-    private fun hideSystemUIAndNavigation(activity: Activity) {
-        val decorView: View = activity.window.decorView
-        decorView.systemUiVisibility =
-            (View.SYSTEM_UI_FLAG_IMMERSIVE
-                    // Set the content to appear under the system bars so that the
-                    // content doesn't resize when the system bars hide and show.
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Hide the nav bar and status bar
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
 }
