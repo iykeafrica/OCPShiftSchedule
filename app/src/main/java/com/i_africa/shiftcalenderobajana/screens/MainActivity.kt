4945475a339109ca47d9a62a848f6bf567c167e1
iykeafrica <ikechiucheagwu@gmail.com>
2021-10-11 01:55:02 +0100

    Alarm notification added to layout

---
+++ app/src/main/java/com/i_africa/shiftcalenderobajana/screens/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.i_africa.shiftcalenderobajana.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.i_africa.shiftcalenderobajana.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
